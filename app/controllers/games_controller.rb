5df97fc882c1b93990e6719e418982bff0f6520e
Kate Catlin <kcatlin2@gmail.com>
2018-02-15 17:21:18 +0800

    Adding session score tracking

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,9 +10,11 @@ class GamesController < ApplicationController
   end
 
   def score
+    @current_score = session[:current_user_score] || 0
     valid_answer = true
     @prompt = params[:token]
     @answer = params[:answer]
+    score_to_add = @answer.length
     @game_result = "Congratulations! #{@answer} is a valid English word!"
 
     prompt_to_array = @prompt.split("")
@@ -20,6 +22,7 @@ class GamesController < ApplicationController
     answer_to_array.each { |char|
       if !prompt_to_array.include?(char)
         valid_answer = false
+        score_to_add = 0
         @game_result = "Sorry but #{@answer} can't be build out of #{@prompt}."
       end
     }
@@ -29,8 +32,11 @@ class GamesController < ApplicationController
       user_serialized = open(url).read
       user = JSON.parse(user_serialized)
       if !user["found"]
+        score_to_add = 0
         @game_result = "Sorry but #{@answer} does not seemt o be a valid English word."
       end
     end
+
+    session[:current_user_score] = @current_score + score_to_add
   end
 end
